6a340a20927c6abb4418b25acf51db051ec0a054
yashodipjadhav2005-hash <yashodipjadhav2005@gmail.com>
2025-12-23 17:46:26 +0530

    Skill section final

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const sidebar = document.querySelector(".sidebar");
     toggleBtn.addEventListener("click", () => {
         sidebar.classList.toggle("active");
     });
+
+
+const skillsSection = document.querySelector('#skills');
+    const progressBars = document.querySelectorAll('.skill-progress');
+
+    const skillObserver = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    progressBars.forEach(bar => {
+                        bar.style.width = bar.style.getPropertyValue('--progress');
+                    });
+                    observer.unobserve(entry.target); // animate only once
+                }
+            });
+        },
+        { threshold: 0.4 }
+    );
+
+    skillObserver.observe(skillsSection);
