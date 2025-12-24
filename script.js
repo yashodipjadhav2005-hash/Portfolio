844f20a20d2ccc2e3617334d56b71c4fad61dc9e
yashodipjadhav2005-hash <yashodipjadhav2005@gmail.com>
2025-12-24 11:29:49 +0530

    Contact Section Final

--- script.js
+++ script.js
@@ -24,3 +24,23 @@ const skillsSection = document.querySelector('#skills');
     );
 
     skillObserver.observe(skillsSection);
+
+
+// Succesfull MSG after submit : 
+const contactForm = document.querySelector(".contact-form");
+    const successMessage = document.getElementById("successMessage");
+
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // Show success message
+        successMessage.style.display = "block";
+
+        // Reset form
+        contactForm.reset();
+
+        // Hide message after 5 seconds
+        setTimeout(() => {
+            successMessage.style.display = "none";
+        }, 5000);
+    });
